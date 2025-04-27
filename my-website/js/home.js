453e0b54df93d41df10ca9b7613e2f834f180374
Hansel-0303 <hanselmaestro01@gmail.com>
2025-04-27 21:14:23 +0800

    Update home.js
    changed api key

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '91171e4528f081025616c90c8f71f363';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
